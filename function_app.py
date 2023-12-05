ad109c69cbf045d4301b7e9cebb1b894e6f2be1d
MaesShankar <shankar.maes@student.howest.be>
2023-12-05 09:14:54 +0100

    commit

--- function_app.py
+++ function_app.py
@@ -28,6 +28,8 @@ app = func.FunctionApp()
 
 
 def cosmosdb_trigger(robohub: func.EventHubEvent):
+    print("start function")
+
     try:
         # Parse the JSON data from the Event Hub event
         json_data_str = robohub.get_body().decode('utf-8')
@@ -35,35 +37,35 @@ def cosmosdb_trigger(robohub: func.EventHubEvent):
         print('Python EventHub trigger processed an event: %s', json_data)
         # Python EventHub trigger processed an event: %s {'machine': {'temperature': 104.53948152349868, 'pressure': 10.51715612293023}, 'ambient': {'temperature': 20.860264751475583, 'humidity': 24}, 'timeCreated': '2023-11-29T09:52:12.8191216Z'}
 
-        if 'id' not in json_data:
-            json_data['id'] = str(uuid.uuid4())  # Generate a unique identifier
-        if 'tempid' not in json_data:
-            json_data['tempid'] = "SimulatedTemp"  # Generate a unique identifier
+        # if 'id' not in json_data:
+        #     json_data['id'] = str(uuid.uuid4())  # Generate a unique identifier
+        # if 'tempid' not in json_data:
+        #     json_data['tempid'] = "SimulatedTemp"  # Generate a unique identifier
 
-        machine_temperature = json_data['machine']['temperature']
-        print(type(machine_temperature))
-        machine_pressure = json_data['machine']['pressure']
-        print(type(machine_pressure))
-        ambient_temperature = json_data['ambient']['temperature']
-        print(type(ambient_temperature))
-        ambient_humidity = json_data['ambient']['humidity']
-        print(type(ambient_humidity))
-        time_created = json_data['timeCreated']
-        print(type(time_created))
+        # machine_temperature = json_data['machine']['temperature']
+        # print(type(machine_temperature))
+        # machine_pressure = json_data['machine']['pressure']
+        # print(type(machine_pressure))
+        # ambient_temperature = json_data['ambient']['temperature']
+        # print(type(ambient_temperature))
+        # ambient_humidity = json_data['ambient']['humidity']
+        # print(type(ambient_humidity))
+        # time_created = json_data['timeCreated']
+        # print(type(time_created))
 
-        # Create a document to be inserted into Cosmos DB
-        document = {
-            'id': json_data['id'],
-            'tempid': json_data['tempid'],
-            'machine_temperature': machine_temperature,
-            'machine_pressure': machine_pressure,
-            'ambient_temperature': ambient_temperature,
-            'ambient_humidity': ambient_humidity,
-            'time_created': time_created
-        }
+        # # Create a document to be inserted into Cosmos DB
+        # document = {
+        #     'id': json_data['id'],
+        #     'tempid': json_data['tempid'],
+        #     'machine_temperature': machine_temperature,
+        #     'machine_pressure': machine_pressure,
+        #     'ambient_temperature': ambient_temperature,
+        #     'ambient_humidity': ambient_humidity,
+        #     'time_created': time_created
+        # }
 
-        # Insert the document into Cosmos DB
-        container_client.upsert_item(document)
+        # # Insert the document into Cosmos DB
+        # container_client.upsert_item(document)
 
         # print('Data inserted into Cosmos DB: %s', document)
 
